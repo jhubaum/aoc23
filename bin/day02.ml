e32712b3d2308786fa1bce2b7f51f382bf53e88f
Johannes Huwald <johannes.huwald@gmail.com>
2023-12-02 21:18:07 +0100

    Day 2 done

--- bin/day02.ml
+++ bin/day02.ml
@@ -35,7 +35,25 @@ let game_is_possible bag game =
   let check_round round = List.for_all check_elem round in
   List.for_all check_round game.rounds;;
 
+let minimum_bag game =
+  let rec for_elem bag color amount = match bag with
+  | [] -> [(color, amount)]
+  | (c, a) :: t when c = color && a < amount -> (color, amount) :: t 
+  | (c, a) :: t when c = color -> (c, a) :: t
+  | h :: t -> h :: (for_elem t color amount) in
+  let for_round bag round = List.fold_left (fun bag r -> for_elem bag (fst r) (snd r)) bag round in
+  List.fold_left (fun bag r -> for_round bag r) [] game.rounds;;
+
+let power_of_bag bag =
+  let amount_for_color color = Option.value
+    (Option.map snd (List.find_opt (fun e -> (fst e) = color) bag)) 
+    ~default: 0 in
+  (amount_for_color "red") * (amount_for_color "blue") * (amount_for_color "green");;
+
+
 let () = 
   let games = List.map game_of_string (Aoc23.read_input "input/02.txt") in
   let possible_games = List.filter (game_is_possible part_one_bag) games in
-  Printf.printf "Part 1: %d\n" (List.fold_left (fun sum game -> sum + game.id) 0 possible_games);;
+  let minimum_bags = List.map minimum_bag games in
+  Printf.printf "Part 1: %d\n" (List.fold_left (fun sum game -> sum + game.id) 0 possible_games);
+  Printf.printf "Part 2: %d\n" (List.fold_left (+) 0 (List.map power_of_bag minimum_bags));;
